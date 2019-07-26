0a31147733e21f891ff8d889a9ee3df787cd004b
Andrii Shpytko <andrey891109@gmail.com>
2019-07-26 22:12:12 +0300

    add two methods in the class 'Calculator.java':subtraction(...), multiplication(...)

--- src/main/java/com/edu/Calculator.java
+++ src/main/java/com/edu/Calculator.java
@@ -1,8 +1,16 @@
 package com.edu;
 
 public class Calculator {
-    
+
     public double addition(double number1, double number2) {
         return number1 + number2;
+    }
+
+    public double subtraction(double number1, double number2) {
+        return number1 - number2;
+    }
+
+    public double multiplication(double number1, double number2) {
+        return number1 * number2;
     }
 }
